e6b984e8a0f7f411d034b8946f3a7c771b645e61
Christian de Balorre <35331735+dm-cdb@users.noreply.github.com>
2026-01-12 21:10:25 +0100

    Reduce delay in loop for quicker analog comparator response
    Updated delay in loop from 500ms to 100ms for faster response.

--- atmega328_analaog_comparator/Nano_comparator.ino
+++ atmega328_analaog_comparator/Nano_comparator.ino
@@ -13,10 +13,10 @@ void setup() {
   DIDR1 = (1 << AIN0D) | (1 << AIN1D);
 
   // Analog Comparator setup:
-  // ACBG = 0 → use AIN1 as negative input
+  // ACBG = 0 → use external voltage on AIN0 ; when set, use internal 1,1V ref voltage.
   // ACIE = 0 → no interrupt
   // ACIC = 0 → not connected to Timer1
-  // ACIS1:0 = 00 → comparator enabled, interrupt disabled anyway
+  // ACIS1:0 = 00 → would set an interrupt on comparator toggle, but we don't use interrupts here.
   ACSR = 0x00;
 
   Serial.println("Analog Comparator ready");
@@ -34,5 +34,6 @@ void loop() {
     digitalWrite(2, 0);
   }
 
-  delay(500);
+  delay(100);
+
 }
